fc5a4661babb5c7c93a61d3aefd4e29893cfded5
Eyad_Aljlal <96502377+EyadAljlal@users.noreply.github.com>
2026-06-14 21:37:00 +0300

    upgrade 2.0.1

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const translations = {
 
 function downloadApk() {
     if (/Android/i.test(navigator.userAgent)) {
-        window.location.href = "almofeed-app-releaseV2.0.0.apk";
+        window.location.href = "almofeed-app-releaseV2.0.1.apk";
     } else {
         alert("يرجى فتح الموقع من جهاز Android لتحميل التطبيق.");
     }
