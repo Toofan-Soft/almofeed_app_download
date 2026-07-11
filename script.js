6f9b78b44a780841cf041b3004afa793b3ab3240
Eyad_Aljlal <96502377+EyadAljlal@users.noreply.github.com>
2026-07-11 21:56:41 +0300

    first version start v 1.0.1
    ..

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const translations = {
 
 function downloadApk() {
     if (/Android/i.test(navigator.userAgent)) {
-        window.location.href = "app-arm64-v8a-release-v2.0.9.apk";
+        window.location.href = "app-arm64-v8a-release-v1.0.1.apk";
     } else {
         alert("يرجى فتح الموقع من جهاز Android لتحميل التطبيق.");
     }
